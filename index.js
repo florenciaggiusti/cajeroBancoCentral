f1a30add17805559ec57a84e667ab440e5fb07ce
Florencia Giusti <122124657+florenciaggiusti@users.noreply.github.com>
2023-03-18 02:19:59 +0000

    creacion de cuenta de ahorro

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
 import {CuentaCorriente} from './CuentaCorriente.js';
+import {CuentaAhorro} from './CuentaAhorro.js';
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
@@ -8,19 +9,7 @@ const cliente2 = new Cliente('María','16979808','8989');
 const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
 const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
 
-let saldo = cuentaDeLeonardo.verSaldo(); 
+const cuentaAhorroLeonardo = new CuentaAhorro(cliente, '9985', '001', 0);
 
-// saldo = cuentaDeLeonardo.depositoEnCuenta(150);
-// cuentaDeLeonardo.transferirParaCuenta(parametroValor,cuentaDeMaria);
-
-const saldoMaria = cuentaDeMaria.verSaldo();
-
-console.log('Cuenta de Maria',cuentaDeMaria);
-
-console.log('El Saldo actual (cuentaMaria) '+saldoMaria);
-
-const saldoLeonardo = cuentaDeLeonardo.verSaldo();
-console.log('El Saldo actual (cuentaLeonardo) '+saldoLeonardo);
-console.log(CuentaCorriente.cantidadCuentas);
-
-console.log(CuentaCorriente.cantidadCuentas);
+console.log(cuentaDeLeonardo);
+console.log(cuentaAhorroLeonardo);
