cd96de47cdfcea3af5d358e173f4f210be6ed7d5
Florencia Giusti <122124657+florenciaggiusti@users.noreply.github.com>
2023-03-28 00:10:10 +0000

    Initial commit

---
+++ SistemaAutenticacion.js
@@ -0,0 +1,8 @@
+export class SistemaAutenticacion {
+    static login(usuario,clave) {
+      if("autenticable" in usuario && usuario.autenticable instanceof Function)
+         return usuario.autenticable(clave);
+      else
+        return false;
+    }
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+/*Importación de clases*/
+import {Cliente} from './Cliente.js'
+import { Empleado } from './Empleados/Empleado.js';
+import { Gerente } from './Empleados/Gerente.js';
+import { Director } from './Empleados/Director.js';
+import { SistemaAutenticacion } from './SistemaAutenticacion.js';
+
+//const cliente2 = new Cliente('María','16979808','8989');
+
+const empleado = new Empleado('Juan Perez','1234343',10000);
+empleado.asignarClave('12345');
+console.log(SistemaAutenticacion.login(empleado,'12345'));
+
+const gerente = new Gerente('Pedro Rivas','232344',12000);
+gerente.asignarClave('655');
+console.log(SistemaAutenticacion.login(gerente,'655'));
+
+//const director = new Director('Elena Moreno','232323',15000);
+
+const cliente = new Cliente('Leonardo','13804050','123224');
+cliente.asignarClave('01111');
+console.log(SistemaAutenticacion.login(cliente,'01111'));
